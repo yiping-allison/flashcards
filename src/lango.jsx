2c296ac5bc2fb773254a515a8b33319abd52fcbc
Yiping Su <ypsu@ucdavis.edu>
2019-05-20 23:30:29 +0000

    added basic cards jsx

--- src/lango.jsx
+++ src/lango.jsx
@@ -1,7 +1,46 @@
 'use strict';
 
+function Card(props) {
+	return <div className = "textCard">
+			{ props.children }
+		</div>;
+}
+
+function Txt(props) {
+	if (props.phrase == undefined) {
+		return <p>Text missing</p>;
+	} else 
+		return <p> {props.phrase}</p>;
+	}
+
+class CreateCardMain extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { opinion: "Life is a bowl of cherries" }
+		this.checkReturn = this.checkReturn.bind(this);
+	}
+
+	render() { return (
+		<main>
+		<Card>
+			<textarea id="inputEng" onKeyPress={this.checkReturn} />
+		</Card>
+		<Card>
+			<Txt phrase={this.state.opinion} />
+		</Card>
+		</main>
+		);
+	} // end of render function
+
+	checkReturn(event) {
+		if (event.charCode == 13) {
+			let newPhrase = document.getElementById("inputEng").value;
+			this.setState({opinion: newPhrase});
+			}
+		}
+} // end class
+
 ReactDOM.render(
-	<h1>Hello World!</h1>,
+	<CreateCardMain />,
 	document.getElementById('root')
 );
-
